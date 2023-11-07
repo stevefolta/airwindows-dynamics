43725644360b1479084a15e67dc92ab442bd1673
Steve Folta <steve@folta.net>
2023-11-06 22:02:48 -0800

    Label: DEBUG_LABEL_BOUNDARIES.

--- CairoWidgets/Label.cpp
+++ CairoWidgets/Label.cpp
@@ -7,6 +7,13 @@ void Label::paint()
 	auto cairo = gui->cairo();
 	cairo_save(cairo);
 
+#ifdef DEBUG_LABEL_BOUNDARIES
+	cairo_rectangle(cairo, rect.x, rect.y, rect.width, rect.height);
+	cairo_set_source_rgb(cairo, 0.0, 0.0, 0.0);
+	cairo_set_line_width(cairo, 1.0);
+	cairo_stroke(cairo);
+#endif
+
 	cairo_select_font_face(cairo, (font ? font : gui->default_font()), CAIRO_FONT_SLANT_NORMAL, font_weight);
 	cairo_set_font_size(cairo, rect.height);
 	cairo_font_extents_t font_extents;
